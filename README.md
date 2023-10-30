9c67a249ce08a74d5f299bfabc4b408a65577df0
Tatyana <quininthenorth@gmail.com>
2023-10-30 15:56:26 +0400

    2. Создания чата на Rest API

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 **The goal of this project is to create a messenger for communication.**
 
-You can create rooms with other users. Rooms can be designed for two users or for many. 
+You can create rooms with other persons. Rooms can be designed for two persons or for many. 
---
+++ src/main/java/ru/job4j/chat/controller/PersonController.java
@@ -0,0 +1,55 @@
+package ru.job4j.chat.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.chat.model.Person;
+import ru.job4j.chat.repository.PersonRepository;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class PersonController {
+    private final PersonRepository persons;
+//    private final BCryptPasswordEncoder encoder;
+
+    public PersonController(PersonRepository persons
+//            , BCryptPasswordEncoder encoder
+    ) {
+        this.persons = persons;
+//        this.encoder = encoder;
+    }
+
+    @GetMapping("/")
+    public List<Person> findAll() {
+        return (List<Person>) persons.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable int id) {
+        var person = this.persons.findById(id);
+        return new ResponseEntity<>(
+                person.orElse(new Person()),
+                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody Person person) {
+//        person.setPassword(encoder.encode(person.getPassword()));
+        persons.save(person);
+    }
+    @PutMapping("/")
+    public ResponseEntity<Void> update(@RequestBody Person person) {
+        this.persons.save(person);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable int id) {
+        Person person = new Person();
+        person.setId(id);
+        this.persons.delete(person);
+        return ResponseEntity.ok().build();
+    }
+}
